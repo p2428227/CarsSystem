d48103d64c85f5e7d4825b5bd6059dc3d9839bec
Joseph Rennocks <P2440738@my365.dmu.ac.uk>
2020-02-18 16:31:29 +0000

    I hate VS

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsCar ACar = new clseCar();
+            Boolean TestData = true;
+
+            ACar.Active = TestData;
+
+            Assert.AreEqual(ACar.Active, TestData);
+        }
+    }
+}
