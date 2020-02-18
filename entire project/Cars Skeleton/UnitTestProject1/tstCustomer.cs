49a7920494d9f886a86a7a2b81e263bacc900e06
Charlotte Lincoln <P2428227@my365.dmu.ac.uk>
2020-02-18 16:29:55 +0000

    add customer class and test

---
+++ entire project/Cars Skeleton/UnitTestProject1/tstCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CarsClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstantOk()
+        {
+            //create an instance of the class we wnat to create
+            clsCustomer AnCustomer = new clsCustomer();
+
+            //test to see if it exists 
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
