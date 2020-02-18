14d4b9ca2c174137c33d165ee519fcb135a56060
Charlotte Lincoln <P2428227@my365.dmu.ac.uk>
2020-02-18 17:07:48 +0000

    add cls and test for customer

---
+++ entire project/Cars Skeleton/UnitTestProject1/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CarsClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
