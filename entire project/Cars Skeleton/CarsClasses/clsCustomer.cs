9ff88276753463bfb47b95202477a662ef82bcba
Charlotte Lincoln <P2428227@my365.dmu.ac.uk>
2020-02-21 13:58:11 +0000

    customer test

--- entire project/Cars Skeleton/CarsClasses/clsCustomer.cs
+++ entire project/Cars Skeleton/CarsClasses/clsCustomer.cs
@@ -1,6 +1,16 @@
-﻿namespace CarsClasses
+﻿using System;
+
+namespace CarsClasses
 {
     public class clsCustomer
     {
+        public bool Active { get; set; }
+        public DateTime dateAdded { get; set; }
+        public int customerID { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public long mobileNumber { get; set; }
+        public string address { get; set; }
+        public bool loyaltyDiscount { get; set; }
     }
 }
