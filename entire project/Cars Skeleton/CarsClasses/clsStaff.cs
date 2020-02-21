cd43fd8160fa460e60ddac3e13e4641ac1468369
Jack Riches <P2440947@my365.dmu.ac.uk>
2020-02-21 14:00:10 +0000

    Finished Staff class and test class

--- entire project/Cars Skeleton/CarsClasses/clsStaff.cs
+++ entire project/Cars Skeleton/CarsClasses/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace CarsClasses
+﻿using System;
+
+namespace CarsClasses
 {
     public class clsStaff
     {
+        public string fullName { get; set; }
+        public int staffNumber { get; set; }
+        public string emailAddress { get; set; }
+        public float hours { get; set; }
+        public DateTime joinDate { get; set; }
+        public int bankAccountNumber { get; set; }
+        public bool staffDiscount { get; set; }
     }
 }
--- entire project/Cars Skeleton/UnitTestProject1/tstStaff.cs
+++ entire project/Cars Skeleton/UnitTestProject1/tstStaff.cs
@@ -13,5 +13,75 @@ namespace UnitTestProject1
             clsStaff aStaff = new clsStaff();
             Assert.IsNotNull(aStaff);
         }
+
+        [TestMethod]
+        public void StaffNumberOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            int testData = 854904354;
+            aStaff.staffNumber = testData;
+
+            Assert.AreEqual(aStaff.staffNumber, testData);
+        }
+
+        [TestMethod]
+        public void FullNamePropertyOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            string testData = "Jason Mars";
+            aStaff.fullName = testData;
+
+            Assert.AreEqual(aStaff.fullName, testData);
+        }
+
+        [TestMethod]
+        public void EmailAddressPropertyOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            string testData = "Jason@Mars.com";
+            aStaff.emailAddress = testData;
+
+            Assert.AreEqual(aStaff.emailAddress, testData);
+        }
+
+        [TestMethod]
+        public void HoursPropertyOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            float testData = 17.5f;
+            aStaff.hours = testData;
+
+            Assert.AreEqual(aStaff.hours, testData);
+        }
+
+        [TestMethod]
+        public void JoinDatePropertyOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            DateTime testData = DateTime.Now.Date;
+            aStaff.joinDate = testData;
+
+            Assert.AreEqual(aStaff.joinDate, testData);
+        }
+
+        [TestMethod]
+        public void BankAccountNumberPropertyOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            int testData = 875439543;
+            aStaff.bankAccountNumber = testData;
+
+            Assert.AreEqual(aStaff.bankAccountNumber, testData);
+        }
+
+        [TestMethod]
+        public void StaffDiscountPropertyOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            bool testData = true;
+            aStaff.staffDiscount = testData;
+
+            Assert.AreEqual(aStaff.staffDiscount, testData);
+        }
     }
 }
