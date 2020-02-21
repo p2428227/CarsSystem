953f6aabf84bbb9dddc41737e26b6e617c742c4c
Charlotte Lincoln <P2428227@my365.dmu.ac.uk>
2020-02-21 13:48:37 +0000

    test

--- entire project/Cars Skeleton/CarsClasses/clsCustomer.cs
+++ entire project/Cars Skeleton/CarsClasses/clsCustomer.cs
@@ -2,5 +2,13 @@
 {
     public class clsCustomer
     {
+        public bool Active { get; set; }
+        public object dateAdded { get; set; }
+        public int customerID { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public long mobileNumber { get; set; }
+        public string address { get; set; }
+        public bool loyaltyDiscount { get; set; }
     }
 }
--- entire project/Cars Skeleton/UnitTestProject1/tstCustomer.cs
+++ entire project/Cars Skeleton/UnitTestProject1/tstCustomer.cs
@@ -7,6 +7,8 @@ namespace UnitTestProject1
     [TestClass]
     public class tstCustomer
     {
+      
+
         [TestMethod]
         public void InstanceOk()
         {
@@ -15,114 +17,88 @@ namespace UnitTestProject1
             //test to see that it exist
             Assert.IsNotNull(AnCustomer);
         }
-    }
 
-    [TestMethod]
-    public void OrderIDPropertyOK() 
-    {
-        //check if ID exists
-        clsOrder AnOrder = new clsOrder();
-        //test to see if ID is entered
-        //create test data
-        int TestData = 1;
-        //assign data to property
-        AnOrder.OrderId = TestData;
-        //test that the values are the same
-        Assert.AreEqual(AnOrder.OrderId, TestData);
-    }
-    [TestMethod]
-    public void CustomerIDOK()
-    {
-        //check if Customer ID exists
-        clsOrder AnOrder = new clsOrder();
-        //test to see if ID is entered
-        //create test data
-        int TestData = 1;
-        //assign data to property
-        AnOrder.CustomerId = TestData;
-        //test that the values are the same
-        Assert.AreEqual(AnOrder.CustomerId, TestData);
-    }
-    [TestMethod]
-    public void StaffIDOK()
-    {
-        //check if ID exists
-        clsOrder AnOrder = new clsOrder();
-        //test to see if ID is entered
-        //create test data
-        int TestData = 1;
-        //assign data to property
-        AnOrder.StaffId = TestData;
-        //test that the values are the same
-        Assert.AreEqual(AnOrder.StaffId, TestData);
-    }
-    [TestMethod]
-    public void vinNumberOK()
-    {
-        //check if ID exists
-        clsOrder AnOrder = new clsOrder();
-        //test to see if ID is entered
-        //create test data
-        int TestData = 1;
-        //assign data to property
-        AnOrder.vinNumber = TestData;
-        //test that the values are the same
-        Assert.AreEqual(AnOrder.vinNumber, TestData);
-    }
-    [TestMethod]
-    public void locationOK()
-    {
-        clsOrder AnOrder = new clsOrder();
-        //create test data
-        string TestData = "TestLocation";
-        //set location to test data
-        AnOrder.location = TestData;
-        //test if they match
-        Assert.AreEqual(AnOrder.location, TestData);
-    }
-    [TestMethod]
-    public void FinanceOK()
-    {
-        clsOrder AnOrder = new clsOrder();
-        //create test data
-        Boolean TestData = true;
-        //set location to test data
-        AnOrder.Finance = TestData;
-        //test if they match
-        Assert.AreEqual(AnOrder.Finance, TestData);
-    }
-    [TestMethod]
-    public void paymentStatusOK()
-    {
-        clsOrder AnOrder = new clsOrder();
-        //create test data
-        Boolean TestData = true;
-        //set location to test data
-        AnOrder.paymentStatus = TestData;
-        //test if they match
-        Assert.AreEqual(AnOrder.paymentStatus, TestData);
-    }
-    [TestMethod]
-    public void DateOK()
-    {
-        clsOrder AnOrder = new clsOrder();
-        //create test data
-        DateTime TestData = DateTime.Now;
-        //set location to test data
-        AnOrder.Date = DateTime.Now;
-        //test if they match
-        Assert.AreEqual(AnOrder.Date, TestData);
-    }
-    [TestMethod]
-    public void PriceOK()
-    {
-        clsOrder AnOrder = new clsOrder();
-        //create test data
-        Decimal TestData = 100;
-        //set location to test data
-        AnOrder.Price = TestData;
-        //test if they match
-        Assert.AreEqual(AnOrder.Price, TestData);
-    }
+        [TestMethod]
+        public void ActivePropertyOk()
+        {
+            //create an istance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //Assign the data to the property
+            AnCustomer.Active = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnCustomer.Active, TestData);
+        }
 
+        [TestMethod]
+        public void dateAddedPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data and assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign data to property
+            AnCustomer.dateAdded = TestData;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.dateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void customerIDPropertyOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Int32 TestData = 20423;
+            AnCustomer.customerID = TestData;
+            Assert.AreEqual(AnCustomer.customerID, TestData);
+
+        }
+
+        [TestMethod]
+        public void fullNamePropertyOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            string TestData = "John Smith";
+            AnCustomer.fullName = TestData;
+            Assert.AreEqual(AnCustomer.fullName, TestData);
+        }
+
+        [TestMethod]
+        public void emailPropertyOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            String TestData = "someone@gmail.com";
+            AnCustomer.email = TestData;
+            Assert.AreEqual(AnCustomer.email, TestData);
+        }
+
+        [TestMethod]
+        public void mobileNumberPropertyOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Int64 TestData = 07763512044;
+            AnCustomer.mobileNumber = TestData;
+            Assert.AreEqual(AnCustomer.mobileNumber, TestData);
+        }
+
+        [TestMethod]
+        public void addressPropertyOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            String TestData = "1 new street, Leicester, Le3 7XL";
+            AnCustomer.address = TestData;
+            Assert.AreEqual(AnCustomer.address, TestData);
+        }
+
+        [TestMethod]
+        public void loyaltydiscountOk()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Boolean TestData = true;
+            AnCustomer.loyaltyDiscount = TestData;
+            Assert.AreEqual(AnCustomer.loyaltyDiscount, TestData);
+        }
     }
+}
+
+
